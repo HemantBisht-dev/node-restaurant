691908e90308717af278290e370e0da1fcaef47b
Hemant Bisht <hmntbst2210@gmail.com>
2024-10-28 23:14:07 +0530

    applied log middleware

--- server.js
+++ server.js
@@ -3,21 +3,37 @@ const db = require("./db");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
-require('dotenv').config();
+require("dotenv").config();
 
-app.get("/", function (req, res) {
+// middleware functions ---> it's a way to add a extra functionality to a application
+
+// logRequest --> it will log (save) the number of user comes in our application
+
+const logRequest = (req, res, next) => {
+  console.log(
+    `${new Date().toLocaleString()} Request Made to ${req.originalUrl}`
+  );
+  next(); // move on next phase
+};
+
+// apply middleware to all Routes
+app.use(logRequest) // this line tells Express to use this middleware for all routes
+
+
+// Define Routes
+app.get("/" , function (req, res) {
   res.send("Welcome to our restaurant");
 });
 
 // import the router files
 const personRoutes = require("./routes/personRoutes");
-const menuRoutes = require('./routes/menuRoutes');
+const menuRoutes = require("./routes/menuRoutes");
 
 // use the routers
 app.use("/person", personRoutes);
-app.use('/menu',menuRoutes)
+app.use("/menu", menuRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // listen to port 3000
 app.listen(PORT, () => {
